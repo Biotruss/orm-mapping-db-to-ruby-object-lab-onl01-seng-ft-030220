f0ef4eb801fe4658308e90ba863197e57023eeb3
Craig Christopher Messi <craig.messi@gmail.com>
2020-04-09 05:23:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,9 @@ class Student
       FROM students
     SQL
       
-    DB[:conn].execute(sql).map
-      
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
     end
-    # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
