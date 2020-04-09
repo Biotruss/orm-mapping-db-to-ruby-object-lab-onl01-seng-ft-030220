eedebd0a21581969df56ec350b19b8df7c971c6e
Craig Christopher Messi <craig.messi@gmail.com>
2020-04-09 05:50:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,11 +58,8 @@ class Student
   end
   
   def self.first_X_students_in_grade_10
-    sql = 'SELECT * FROM students WHERE grade = 10'
-    
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, x)
   end
   
   def save
